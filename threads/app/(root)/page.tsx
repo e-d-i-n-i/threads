674879b964d4be3d38c52f7105f3dfc94368c646
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2024-02-01 13:13:15 +0300

    Clerk Implementation

--- threads/app/(root)/page.tsx
+++ threads/app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <ClerkProvider>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </ClerkProvider>
   );
 }
