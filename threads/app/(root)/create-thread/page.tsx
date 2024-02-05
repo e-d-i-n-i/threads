5c1ffc586307b77b1821d047b6049d5403ea4868
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2024-02-06 00:10:25 +0300

    Profile Page Imp

--- threads/app/(root)/create-thread/page.tsx
+++ threads/app/(root)/create-thread/page.tsx
@@ -8,8 +8,8 @@ async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
-  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
+  
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
