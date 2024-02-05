2affcb210bc353a1b97cc01ff8dcba1b8fba0d4c
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2024-02-05 22:14:00 +0300

    Reply & Comment
    > fixed some bugs on onboarding and next js config and accountProfile too

--- threads/app/(root)/thread/[id]/page.tsx
+++ threads/app/(root)/thread/[id]/page.tsx
@@ -3,6 +3,8 @@ import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Comment from "@/components/forms/Comment";
+
 
 const Page = async ({ params }: { params: { id: string } }) => {
 
@@ -30,6 +32,29 @@ const Page = async ({ params }: { params: { id: string } }) => {
                     comments={thread.children}
 
                 />
+            </div>
+            <div className="mt-7">
+                <Comment 
+                    threadId = {thread.id}
+                    currentUserImg ={userInfo.image}
+                    currentUserId = {JSON.stringify(userInfo._id)}
+                />
+            </div>
+            <div className="mt-10 ">
+                    {thread.children.map((childItem:any) => 
+                     <ThreadCard
+                     key={childItem._id}
+                     id={childItem._id}
+                     currentUserId={user?.id || ""}
+                     parentId={childItem.parentId}
+                     content={childItem.text}
+                     author={childItem.author}
+                     community={childItem.community}
+                     createdAt={childItem.createdAt}
+                     comments={childItem.children}
+                    isComment
+                 />
+                    )}
             </div>
         </section>
     );
