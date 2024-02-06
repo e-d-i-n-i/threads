0aa8cc520aecc7f0b4b87aad54f3762bc997cddc
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2024-02-06 22:43:55 +0300

    Activity Page Imp

--- threads/lib/actions/user.actions.ts
+++ threads/lib/actions/user.actions.ts
@@ -84,58 +84,87 @@ export async function fetchUserPosts(userId: string) {
         }
 
       })
-      return threads;
-  } catch (error:any) {
+    return threads;
+  } catch (error: any) {
     throw new Error(`Failed to fetch user posts: ${error.message}`)
   }
 }
 
 export async function fetchUsers({
   userId,
-  searchString="",
-  pageNumber=1,
-  pageSize=20,
-  sortBy="desc"
-}:{
-  userId:string;
-  searchString?:string;
-  pageNumber?:number;
-  pageSize?:number;
-  sortBy?:SortOrder;
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc"
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
 }) {
   try {
     connectToDB();
-    
+
     const skipAmount = (pageNumber - 1) * pageSize;
 
-    const regex = new RegExp(searchString,"i");
+    const regex = new RegExp(searchString, "i");
 
-    const query:FilterQuery<typeof User> = {
-        id: {$ne: userId}
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId }
     }
 
-    if(searchString.trim() !== ''){
-        query.$or = [
-          {username:{$regex: regex}},
-          {name:{$regex:regex}} 
-        ]
+    if (searchString.trim() !== '') {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } }
+      ]
     }
 
-    const sortOptions = {createdAt: sortBy};
+    const sortOptions = { createdAt: sortBy };
     const usersQuery = User.find(query)
-    .sort(sortOptions)
-    .skip(skipAmount)
-    .limit(pageSize);
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
 
     const totalUsersCount = await User.countDocuments(query);
     const users = await usersQuery.exec();
 
     const isNext = totalUsersCount > skipAmount + users.length;
 
-    return {users, isNext};
+    return { users, isNext };
 
-  } catch (error:any) {
-        throw new Error(`Failed to fetch users: ${error.message}`)
+  } catch (error: any) {
+    throw new Error(`Failed to fetch users: ${error.message}`)
 
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field of each user thread
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    // Find and return the child threads (replies) excluding the ones created by the same user
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
   }
 }
